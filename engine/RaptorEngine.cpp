fe91f621469235faa3dad27835d5dfc92e560271
Dario Freddi <drf54321@gmail.com>
2008-09-10 14:36:00 +0200

    Adding myself to copyright headers

--- engine/RaptorEngine.cpp
+++ engine/RaptorEngine.cpp
@@ -1,6 +1,7 @@
 /* This file is part of the KDE project
 
    Copyright (C) 2008 Lukas Appelhans <l.appelhans@gmx.de>
+   Copyright (C) 2008 Dario Freddi <drf@kdemod.ath.cx>
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
