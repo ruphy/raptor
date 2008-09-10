152142de145991ee621cb5a12d2110ec252f2527
Rafael <menda@mac.(none)>
2008-09-10 15:40:44 +0200

    modified:   engine/RaptorEngine.cpp 	modified:   engine/RaptorEngine.h 	modified:   raptor.cpp 	modified:   raptor.h

--- raptor.cpp
+++ raptor.cpp
@@ -1,4 +1,12 @@
- 
+ /* This file is part of the KDE project
+
+   Copyright (C) 2008 Lukas Appelhans <l.appelhans@gmx.de>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+*/
 #include "raptor.h"
 #include <QPainter>
 #include <QFontMetrics>
