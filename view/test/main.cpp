b3f42e38853ce474d8385460edb1850101638b82
Alessandro Diaferia <alediaferia@gmail.com>
2008-09-14 15:31:20 +0200

    testview app included

---
+++ view/test/main.cpp
@@ -0,0 +1,51 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2008 Alessandro Diaferia <alediaferia@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/ 
+
+#include <KApplication>
+#include <KAboutData>
+#include <KLocale>
+#include <KCmdLineArgs>
+#include <KXmlGuiWindow>
+
+#include "view/raptoritemsview.h"
+#include "engine/kickoff/applicationmodel.h"
+
+/**
+ * A simple test application for RaptorItemsView
+ */
+
+int main(int argc, char *argv[])
+{
+    KAboutData aboutData("itemview-test", 0, ki18n("test for item view"),
+                         "0.1", ki18n("test app"),
+                         KAboutData::License_GPL,
+                         ki18n("(c) 2008 Alessandro Diaferia"),
+                         KLocalizedString(), "", "alediaferia@gmail.com");
+    aboutData.addAuthor(ki18n("Alessandro Diaferia"), KLocalizedString(), "alediaferia@gmail.com");
+
+    KCmdLineArgs::init(argc, argv, &aboutData);
+
+    KCmdLineOptions options;
+    KCmdLineArgs::addCmdLineOptions(options);
+    KCmdLineArgs::parsedArgs();
+    KApplication app;
+
+    KXmlGuiWindow *window = new KXmlGuiWindow();
+    RaptorItemsView *itemsView = new RaptorItemsView(window);
+    Kickoff::ApplicationModel *model = new Kickoff::ApplicationModel();
+    itemsView->setModel(model);
+
+    window->setCentralWidget(itemsView);
+
+    window->show();
+
+   return app.exec();
+    
+}
