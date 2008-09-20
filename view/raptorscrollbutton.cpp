d22304827586747e61fa50ecd8241360e3eea9de
Lukas Appelhans <l.appelhans@gmx.de>
2008-09-20 19:42:32 +0200

    this looks good, but the code is hell lol

--- view/raptorscrollbutton.cpp
+++ view/raptorscrollbutton.cpp
@@ -49,6 +49,15 @@ void RaptorScrollButton::paintEvent(QPaintEvent * event)
     QPainter p(this);
     QRectF r(event->rect());
     r.setSize(size());
+    kDebug() << r.size();
+    r.setSize(QSizeF(r.width() * 0.7, r.height() * 0.6));
+    kDebug() << r.size();
+    QPointF point = r.center();
+    kDebug() << point;
+    point.setX(point.x() - r.width() / 4);
+    point.setY(point.y() - r.height() / 4);
+    kDebug() << point;
+    r.moveTo(point);
     if (d->side == Right) {
        d->svg->paint(&p, r, "rightarrow");
     }
