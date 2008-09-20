1604a5fff346efa40d9bce568b2a0e44f51791d1
Lukas Appelhans <l.appelhans@gmx.de>
2008-09-20 20:08:59 +0200

    do some unlogic calculations to place the scrollbutton

--- view/raptorscrollbutton.cpp
+++ view/raptorscrollbutton.cpp
@@ -48,16 +48,8 @@ void RaptorScrollButton::paintEvent(QPaintEvent * event)
     kDebug();
     QPainter p(this);
     QRectF r(event->rect());
-    r.setSize(size());
-    kDebug() << r.size();
-    r.setSize(QSizeF(r.width() * 0.7, r.height() * 0.6));
-    kDebug() << r.size();
-    QPointF point = r.center();
-    kDebug() << point;
-    point.setX(point.x() - r.width() / 4);
-    point.setY(point.y() - r.height() / 4);
-    kDebug() << point;
-    r.moveTo(point);
+    r.setSize(QSizeF(size().width() * 0.7, size().height() * 0.6));
+    r.moveCenter(QPointF(size().width() / 2, size().height() / 2));
     if (d->side == Right) {
        d->svg->paint(&p, r, "rightarrow");
     }
