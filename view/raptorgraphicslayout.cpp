e02b7610e38db0d3d6c64fdd57d080d21bfb9edd
Alessandro Diaferia <alediaferia@gmail.com>
2009-03-16 18:45:53 +0100

    the first layout stuff stub

---
+++ view/raptorgraphicslayout.cpp
@@ -0,0 +1,198 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2009 Alessandro Diaferia <alediaferia@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+#include "raptorgraphicslayout.h"
+
+#include "raptorgraphicsview.h"
+#include "raptormenuitem.h"
+
+#include <QEvent>
+
+#include <KDebug>
+
+class RaptorGraphicsLayout::Private
+{
+public:
+    Private(RaptorGraphicsLayout *q) : q(q), view(0), topMargin(0), leftMargin(0), rightMargin(0), bottomMargin(0)
+    {}
+    ~Private()
+    {}
+
+    RaptorGraphicsView *view;
+    QList<RaptorMenuItem*> items;
+    QList<RaptorMenuItem*> visibleItems;
+
+    qreal topMargin;
+    qreal leftMargin;
+    qreal rightMargin;
+    qreal bottomMargin;
+
+    void layoutItems();
+};
+
+RaptorGraphicsLayout::RaptorGraphicsLayout(RaptorGraphicsView *parent) : QObject(parent), d(new Private(this))
+{
+    d->view = parent;
+}
+
+RaptorGraphicsLayout::~RaptorGraphicsLayout()
+{
+    delete d;
+}
+
+void RaptorGraphicsLayout::setMenuItems(const QList<RaptorMenuItem*> &items)
+{
+    d->items.clear();
+    d->visibleItems.clear();
+    d->items << items;
+}
+
+void RaptorGraphicsLayout::visibleItems() const
+{
+    return d->visibleItems;
+}
+
+void RaptorGraphicsLayout::setContentMargins(qreal left, qreal top, qreal right, qreal bottom)
+{
+    d->topMargin = top;
+    d->leftMargin = left;
+    d->rightMargin = right;
+    d->bottomMargin = bottom;
+}
+
+bool RaptorGraphicsLayout::eventFilter(QObject *obj, QEvent *event)
+{
+    if (obj != d->view) {
+        return false;
+    }
+
+    if (event->type() == QEvent::Resize) {
+        d->layoutItems();
+        return true;
+    }
+
+    return false;
+}
+
+void RaptorGraphicsLayout::invalidate()
+{
+    d->layoutItems();
+}
+
+void RaptorGraphicsLayout::layoutItems()
+{
+    if (d->items.isEmpty()) {
+        d->visibleItems.clear();
+        return;
+    }
+    // NOTE: for each view mode we should setup items individually
+    // WARNING: we suppose a horizontal view
+    // TODO: remove tabs
+    RaptorMenuItem * oldFirst = 0;
+    if (!d->visibleItems.isEmpty()) {
+        oldFirst = d->visibleItems.first();
+    }
+    d->visibleItems.clear();
+//     d->needsAnimation.clear();
+    RaptorGraphicsView::ViewMode mode = d->view->viewMode();
+
+    QRectF rect = d->view->rect();
+    rect.setSize(QSizeF(rect.width() - (d->leftMargin + d->rightMargin), rect.height() - (d->topMargin + d->bottomMargin)));
+    rect.translate(d->topMargin, d->leftMargin);
+
+    if (mode == RaptorGraphicsView::Normal) {
+        kDebug() << "YAY";
+        qreal sizesSum = 0;
+        const qreal size = rect.height();
+        foreach (RaptorMenuItem *item, d->items) {
+            kDebug() << "Set item rect" << QRectF(QPointF(sizesSum, d->topMargin), QSizeF(size, size));
+
+            item->moveTo(QRectF(QPointF(sizesSum, d->topMargin), QSizeF(size, size)));
+            sizesSum += size;
+
+            if (sizesSum - item->rect().width() > rect.width()) {
+                break;
+            }
+            d->visibleItems << item;
+        }
+        if (!d->visibleItems.contains(d->items.last())) {
+            d->items.last()->setRect(QRectF(QPointF(-1 * size,d->topMargin), QSizeF(size, size)));
+        }
+        if (oldFirst && !d->visibleItems.contains(oldFirst)) {
+            kDebug() << "Move the old first outside";
+            oldFirst->moveTo(QRectF(QPointF(!size, d->topMargin), QSizeF(size, size)));
+//             d->needsAnimation << oldFirst;
+        }
+
+//         if (d->scrollTimeLine->state() == QTimeLine::Running) {
+//             d->scrollTimeLine->stop();
+//         }
+//         d->scrollTimeLine->start();
+    }
+
+    else if (mode == RaptorGraphicsView::SingleApp) {
+        RaptorMenuItem *item = d->items.first();
+        item->setRect(QRectF(QPointF(0, 0), rect.size()));//Don't use rect for single-app, we don't need a margin here
+        d->visibleItems << item;
+    }
+
+    else if (mode == RaptorGraphicsView::TwoApps) {
+	qreal x = 0;
+	for (int i = 0; i < 2; i++) {
+            if (d->items.count() <= i) {
+                break;
+            }
+	    RaptorMenuItem *item = d->items[i];
+	    item->setRect(QRectF(QPointF(x, d->topMargin), QSizeF(rect.width() / 2, rect.height())));
+	    x += rect.width() / 2;
+            d->visibleItems << item;
+	}
+     }
+
+     else if (mode == RaptorGraphicsView::Search) {
+	  qreal sizesSum = 0;
+	  int i = 0;
+	  qreal y = d->topMargin;
+
+	  for (; i < 2; i++) { // we place the first two items half sized and in column
+	      RaptorMenuItem *item = d->items[i];
+	      item->setRect(QRectF(QPointF(0, y), QSizeF(rect.height(), rect.height() / 2)));
+	      y += rect.height() / 2;
+              d->visibleItems << item;
+	  }
+	  sizesSum += rect.height();
+
+	  for (; i < d->items.count(); i++) { // now we take care of left items
+	      if (sizesSum > rect.width() - (rect.height())) {
+		  sizesSum = rect.width() - (rect.height());
+		  break;
+	      }
+
+	      if ( d->items.count() - i == 2 ) {
+		  break;
+	      }
+
+	      RaptorMenuItem *item = d->items[i]; 
+	      item->setRect(QRectF(QPointF(sizesSum, d->topMargin), QSizeF(rect.height(), rect.height())));
+	      d->visibleItems << item;
+
+	      sizesSum += rect.height();
+	  }
+
+	  int max = i + 2;
+	  y = d->topMargin;
+	  for (; i < max; i++) { // here we handle the last two items
+	      RaptorMenuItem *item = d->items[i];
+	      item->setRect(QRectF(QPointF(sizesSum, y), QSizeF(rect.height(), rect.height() / 2)));
+	      y += rect.height() / 2;
+              d->visibleItems << item;
+	  }
+    }
+
+}
---
+++ view/raptorgraphicslayout.h
@@ -0,0 +1,39 @@
+/* This file is part of the KDE project
+
+   Copyright (C) 2009 Alessandro Diaferia <alediaferia@gmail.com>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+*/
+#ifndef RAPTORGRAPHICSLAYOUT_H
+#define RAPTORGRAPHICSLAYOUT_H
+
+#include <QObject>
+#include <QList>
+class RaptorGraphicsView;
+class RaptorMenuItem;
+class QEvent;
+
+class RaptorGraphicsLayout : public QObject
+{
+    Q_OBJECT
+public:
+    RaptorGraphicsLayout(RaptorGraphicsView *parent);
+    ~RaptorGraphicsLayout();
+
+    void setMenuItems(const QList<RaptorMenuItem*> items);
+    QList<RaptorMenuItem*> visibleItems() const;
+
+    void setContentMargins(qreal left, qreal top, qreal right, qreal bottom);
+    void invalidate();
+
+    bool eventFilter(QObject *, QEvent *);
+
+private:
+    class Private;
+    Private *d;
+};
+
+#endif
